f350f661b15f25d790f72d800ec75c210c922af4
UnzorZ <73613908+UnzorZ@users.noreply.github.com>
2021-12-07 16:59:09 +0100

    Improved spammer (random delay, random sentences)

--- src/main/java/me/htrewrite/client/module/modules/miscellaneous/ChatModule.java
+++ src/main/java/me/htrewrite/client/module/modules/miscellaneous/ChatModule.java
@@ -5,12 +5,15 @@ import me.htrewrite.client.command.CommandManager;
 import me.htrewrite.client.event.custom.player.PlayerChatEvent;
 import me.htrewrite.client.module.Module;
 import me.htrewrite.client.module.ModuleType;
+import me.htrewrite.exeterimports.mcapi.settings.IntegerSetting;
 import me.htrewrite.exeterimports.mcapi.settings.ModeSetting;
 import me.htrewrite.exeterimports.mcapi.settings.StringSetting;
 import me.htrewrite.exeterimports.mcapi.settings.ToggleableSetting;
+import me.pk2.moodlyencryption.util.RandomString;
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
 import net.minecraft.network.play.client.CPacketChatMessage;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class ChatModule extends Module {
     public static final ModeSetting configMode = new ModeSetting("Config", null, 0, BetterMode.construct("Modules", "Settings"));
@@ -20,6 +23,8 @@ public class ChatModule extends Module {
     /* Suffix */
     public static final ToggleableSetting suffix_enabled = new ToggleableSetting("SuffixEnabled", null, true);
     public static final StringSetting suffix = new StringSetting("Suffix", null, "HTRewrite");
+    public static final ToggleableSetting randomletters = new ToggleableSetting("RandomLetters", null, false);
+    public static final IntegerSetting numberletters = new IntegerSetting("Letters", null, 3, 1, 20);
     /* Color */
     public static final ToggleableSetting color_enabled = new ToggleableSetting("ColorEnabled", null, false);
     public static final ModeSetting color = new ModeSetting("Color", null, 0, BetterMode.construct("YELLOW", "GREEN", "BLUE"));
@@ -32,6 +37,8 @@ public class ChatModule extends Module {
         addOption(setting.setVisibility(a -> configMode.getI() == 0));
         addOption(suffix_enabled.setVisibility(a -> setting.getI() == 0 && configMode.getI() == 0));
         addOption(suffix.setVisibility(a -> setting.getI() == 0 && configMode.getI() == 0));
+        addOption(randomletters.setVisibility(a -> setting.getI() == 0 && configMode.getI() == 0));
+        addOption(numberletters.setVisibility(a -> setting.getI() == 0 && configMode.getI() == 0 && randomletters.isEnabled()));
         addOption(color_enabled.setVisibility(a -> setting.getI() == 1 && configMode.getI() == 0));
         addOption(color.setVisibility(a -> setting.getI() == 1 && configMode.getI() == 0));
         addOption(exceptions.setVisibility(a -> configMode.getI() == 1));
@@ -61,6 +68,8 @@ public class ChatModule extends Module {
             }
             message = colorMsg + message;
         }
+        if (randomletters.isEnabled())
+            message += " " + (new RandomString(numberletters.getValue()).nextString());
 
         mc.player.connection.sendPacket(new CPacketChatMessage(message));
     });
--- src/main/java/me/htrewrite/client/module/modules/miscellaneous/SpammerModule.java
+++ src/main/java/me/htrewrite/client/module/modules/miscellaneous/SpammerModule.java
@@ -5,10 +5,8 @@ import me.htrewrite.client.event.custom.player.PlayerUpdateEvent;
 import me.htrewrite.client.module.Module;
 import me.htrewrite.client.module.ModuleType;
 import me.htrewrite.client.util.TickedTimer;
-import me.htrewrite.exeterimports.mcapi.settings.ModeSetting;
-import me.htrewrite.exeterimports.mcapi.settings.StringSetting;
-import me.htrewrite.exeterimports.mcapi.settings.ToggleableSetting;
-import me.htrewrite.exeterimports.mcapi.settings.ValueSetting;
+import me.htrewrite.exeterimports.mcapi.settings.*;
+import me.pk2.moodlyencryption.util.RandomString;
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
 import net.minecraft.network.play.client.CPacketChatMessage;
@@ -23,6 +21,9 @@ public class SpammerModule extends Module {
     public static final StringSetting spammerFile = new StringSetting("File", null, "spammer.txt");
     public static final ToggleableSetting usePackets = new ToggleableSetting("UsePackets", null, false);
     public static final ValueSetting<Double> delay = new ValueSetting<>("Delay", null, 5d, 1d, 60d);
+    public static final ToggleableSetting randomdelay = new ToggleableSetting("RDelay", null, false);
+    public static final IntegerSetting randomdelayint = new IntegerSetting("RDelayMS", null, 50, 1, 10000);
+    public static final ToggleableSetting randomsentences = new ToggleableSetting("RandomSentences", null, false);
 
     private final String directory = "htRewrite\\spammer\\";
     private File currentFile = null;
@@ -34,6 +35,9 @@ public class SpammerModule extends Module {
         addOption(spammerFile);
         addOption(usePackets);
         addOption(delay);
+        addOption(randomdelay);
+        addOption(randomdelayint.setVisibility(v -> randomdelay.isEnabled()));
+        addOption(randomsentences);
         endOption();
 
         new File(directory).mkdirs();
@@ -105,7 +109,23 @@ public class SpammerModule extends Module {
     @EventHandler
     private Listener<PlayerUpdateEvent> updateEventListener = new Listener<>(event -> {
         int realDelay = delay.getValue().intValue()*20;
+
+        if (randomdelay.isEnabled()) {
+            int min = 0;
+            int max;
+            max = (int) (Math.random() * (randomdelayint.getValue()));
+            if (max == min) {
+                max = (int) (Math.random() * (randomdelayint.getValue()));
+            }
+            min = max;
+            realDelay = (delay.getValue().intValue() + max)*20;
+        }
+
         if(tickedTimer.passed(realDelay)) {
+            int min = 0;
+            int max = 2;
+            int randomnum = (int) (Math.random()*(max-min+1)+min);
+            if (randomnum == 1) {
             switch(mode.getValue()) {
                 case "LIST": {
                     if(listIndex >= lines.length)
@@ -125,7 +145,10 @@ public class SpammerModule extends Module {
                 default:
                     break;
             }
-
+            } else {
+                if (randomsentences.isEnabled())
+                sendChatMessage(new RandomString(8).nextString());
+            }
             tickedTimer.reset();
         }
     });
